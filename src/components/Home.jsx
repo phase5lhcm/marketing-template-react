e588303f1861bbf3a4559c55ca25cf8ba5822ac7
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-19 20:55:44 -0500

    Build hero section TODO - change nav-items color to white

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/HomeStyles.css";
 import NavBar from "./NavBar";
+import Hero from "./Hero";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };

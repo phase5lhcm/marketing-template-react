71456498e6facf847970840b6f1db9df29abc424
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-19 22:32:15 -0500

    Build Pricing component and add media-queries

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/HomeStyles.css";
 import NavBar from "./NavBar";
 import Hero from "./Hero";
+import Pricing from "./Pricing";
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Pricing />
     </div>
   );
 };
